5406dc6e8acf2418e7b4f4559e7fa59df3a05196
Mychel Costa <mychel.costa@sga.pucminas.br>
2021-11-28 18:19:09 -0300

    Tela de login funcional e melhorias de CSS

--- js/validacaocad.js
+++ js/validacaocad.js
@@ -17,7 +17,7 @@ function validacao(){
         alert("Obrigatório informar o seu nome completo!");
         nomecadastro.focus();
         return false;
-    }else if(emailcadastro.value.indexOf('@')== -1 || email.value.indexOf('.') == -1){
+    }else if(emailcadastro.value.indexOf('@')== -1 || emailcadastro.value.indexOf('.') == -1){
         nomecadastro.style.backgroudColor = 'transparent';
         emailcadastrostyle.backgroudColor = "#ff6459";
         alert("Obrigatório informar o email!");
@@ -26,7 +26,7 @@ function validacao(){
     }else if(senha.value.length < 8){
         emailcadastro.style.backgroudColor = 'transparent';
         senha.style.backgroudColor = "#ff6459";
-        alert("Obrigatório informar uma senha!");
+        alert("Obrigatório informar uma senha de no mínimo 8 carácteres!");
         senha.focus();
         return false;
     }else if(valsenha.value != senha.value){
@@ -48,6 +48,19 @@ function validacao(){
         localStorage.setItem("usuario_Cad", JSON.stringify(usuarioCad));
         localStorage.removeItem('email_preCad')
         location.href = 'busca.html'
+    }
+}
+
+function verificarUsu(){
+    var usuarioCad = JSON.parse(localStorage.getItem('usuario_Cad') || '{}');
+
+    var usuarioemail = document.querySelector('#email').value;
+    var usuariosenha = document.querySelector('#senha').value; 
+
+    if (usuarioemail == usuarioCad.email && usuariosenha == usuarioCad.senha){
+        location.href = 'busca.html';
+    }else{
+        alert('⚠️ Usuário não cadastrado ou senha incorreta!');
     }
 }
 
